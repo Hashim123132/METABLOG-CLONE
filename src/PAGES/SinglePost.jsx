a9b5a61afae0f714f0ac06c6e3178f32683e6a3a
Hashim Umar <hashimumarsyed2005@gmail.com>
2024-10-15 01:24:16 +0500

    All files are included

---
+++ src/PAGES/SinglePost.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+
+ const SinglePost = () => {
+  return (
+    <div>
+         <div className=' text-white font-semibold h-[184px] w-[1136px]     flex flex-col items-center mx-auto justify-center  mt-[100px] '>
+                             <div className='flex flex-col '>
+                              <h2 className=' text-xs    bg-[#4B6BFB] rounded-md  mr-auto py-[6px] px-[12px] mb-3'>Technology</h2>
+                              <p className='font-semibold text-4xl w-[800px] mr-auto'>The Impact of Technology on the Workplace: How Technology is Changing</p>
+                              <div className='flex space-x-4 items-center mr-auto pt-5'>
+                                  <img className='w-10 rounded-full ' src="./IMAGES/6891be7496ccabdc2fd0b1a1d8a535d7.png" alt="" />
+                                  <p className='text-[#696A75]'>Tracey Wilson</p>
+                                  <p className='text-[#696A75]'>August 20, 2022</p>
+                              </div>
+                             </div>
+                        </div>
+                                                                 {/* THE DIV AFTER HEAD  ABOVE*/}
+                                
+                                  <div className='flex flex-col items-center'>
+                                    
+                                    <img className='w-[800px] h-[462px]  rounded-xl object-cover' src="./IMAGES/a6a8e0df2280c787cafc780464b002fe.jpeg" alt="" />
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl' >Traveling is an enriching experience that opens up new horizons, exposes us to different cultures, and creates memories that last a lifetime. However, traveling can also be stressful and overwhelming, especially if you don't plan and prepare adequately. In this blog article, we'll explore tips and tricks for a memorable journey and how to make the most of your travels.</p>
+
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl mt-5'>One of the most rewarding aspects of traveling is immersing yourself in the local culture and customs. This includes trying local cuisine, attending cultural events and festivals, and interacting with locals. Learning a few phrases in the local language can also go a long way in making connections and showing respect.</p> 
+                                    
+                                    <h2 className='text-white text-2xl font-semibold mr-[520px] mt-5'>Research Your Destination</h2>
+                                   
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl mt-5'>Before embarking on your journey, take the time to research your destination. This includes understanding the local culture, customs, and laws, as well as identifying top attractions, restaurants, and accommodations. Doing so will help you navigate your destination with confidence and avoid any cultural faux pas.
+                                    </p>
+                                   
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl mt-5'>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.
+                                    </p>
+                                    <h2 className='text-white text-2xl font-semibold mr-[600px] mt-5'>Plan Your Itinerary</h2>
+
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl mt-5'>While it's essential to leave room for spontaneity and unexpected adventures, having a rough itinerary can help you make the most of your time and budget. Identify the must-see sights and experiences and prioritize them according to your interests and preferences. This will help you avoid overscheduling and ensure that you have time to relax and enjoy your journey.</p>
+
+                                    <p className='text-[#BABABF] font-serif w-[804px] text-xl mt-5'>
+                                    Vitae sapien pellentesque habitant morbi tristique. Luctus venenatis lectus magna fringilla. Nec ullamcorper sit amet risus nullam eget felis. Tincidunt arcu non sodales neque sodales ut etiam sit amet.
+                                    </p>
+
+                                    <div className='bg-[#242535] rounded-lg p-[32px] w-[800px] h-[128px] mt-10'>
+                                      <p className='text-white text-[24px] leading-[32px] font-serif'>“ Traveling can expose you to new environments and potential health risks, so it's crucial to take precautions to stay safe and healthy. ”</p>
+                                    </div>
+                                    <div className='flex flex-col justify-center items-center'>
+                                      <img className='w-[800px] h-[462px] rounded-xl object-cover' src="./IMAGES/4ffe2414f1f3b6c944cef034a92ef7f5.jpeg" alt="" />
+                                      <div className='bg-[#242535] rounded-xl p-[32px] w-[750px] h-[100px] mt-10  flex flex-col justify-center items-center'>
+                                      
+                                        <p className='text-[#696A75]'>Advertisement</p>
+                                        <p className='font-bold text-[#696A75] text-[20px] leading-[24px]'>You can place ads</p>
+                                        <p className=' text-[#696A75] text-[18px]'>750x100</p>
+
+                                      </div>
+                                     
+                                      <h2 className='text-white text-2xl font-semibold mr-[485px] mt-5'>Stay Pack Lightly and Smartly</h2>
+                                     
+                                      <p className='font-serif mt-4 text-[#BABABF] text-[20px] leading-[32px] w-[804px]'>Packing can be a daunting task, but with some careful planning and smart choices, you can pack light and efficiently. Start by making a packing list and sticking to it, focusing on versatile and comfortable clothing that can be mixed and matched. Invest in quality luggage and packing organizers to maximize space and minimize wrinkles.</p>
+                                     
+                                      <h2 className='text-white text-2xl font-semibold mr-[565px] mt-5'>Stay Safe and Healthy</h2>
+                                     
+                                      <p className='font-serif mt-4 text-[#BABABF] text-[20px] leading-[32px] w-[804px]'>Traveling can expose you to new environments and potential health risks, so it's crucial to take precautions to stay safe and healthy. This includes researching any required vaccinations or medications, staying hydrated, washing your hands frequently, and using sunscreen and insect repellent. It's also essential to keep your valuables safe and secure and to be aware of your surroundings at all times.</p>
+
+                                      <h2 className='text-white text-2xl font-semibold mr-[400px] mt-5'>Immerse Yourself in the Local Culture</h2>
+                                     
+                                       <p className='font-serif mt-4 text-[#BABABF] text-[20px] leading-[32px] w-[804px]'>One of the most rewarding aspects of traveling is immersing yourself in the local culture and customs. This includes trying local cuisine, attending cultural events and festivals, and interacting with locals. Learning a few phrases in the local language can also go a long way in making connections and showing respect.</p>
+                                       
+                                      <h2 className='text-white text-2xl font-semibold mr-[600px] mt-5'>Capture Memories</h2>
+                                     
+                                     <p className='font-serif mt-4 text-[#BABABF] text-[20px] leading-[32px] w-[804px]'>Finally, don't forget to capture memories of your journey. Whether it's through photographs, journaling, or souvenirs, preserving the moments and experiences of your travels can bring joy and nostalgia for years to come. However, it's also essential to be present in the moment and not let technology distract you from the beauty of your surroundings.</p>
+                                      
+                                      <h2 className='text-white text-2xl font-semibold mr-[680px] mt-5'>Conclusion:</h2>
+                                     
+                                     <p className='font-serif mt-4 text-[#BABABF] text-[20px] leading-[32px] w-[804px]'>Traveling is an art form that requires a blend of planning, preparation, and spontaneity. By following these tips and tricks, you can make the most of your journey and create memories that last a lifetime. So pack your bags, embrace the adventure, and enjoy the ride.</p>
+                                    </div>
+                                  </div>
+                                                                {/* MAIN CONTENT DIV ABOVE*/}
+
+                                  <div className='bg-[#141624] border-t border-b border-solid border-t-slate-800 border-b-slate-800 min-h-screen mt-20 ' >
+                 
+                 <div className='flex justify-center items-center space-x-36 mt-14'>
+
+                 
+                         <div className='p-4 '>
+                           <p className='font-semibold text-white text-[18px]'>About</p>
+                           <p className='text-[#97989F] w-[260px] mt-5'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                           <p className='font-semibold text-white mt-5'>Email : <span className='text-[#97989F]'>info@jstemplate.net</span> </p>
+                           <p className='font-semibold text-white'>Phone : <span className='text-[#97989F]'>880 123 456 789</span> </p>
+
+                       </div>
+                                                                         {/* ABOUT KI DIV ABOVE */}
+                       
+                       <div className='flex flex-col'>
+                         <h1 className='text-white text-[18px] font-semibold'>Quicklink</h1>
+                       <ul className='text-[#BABABF] mt-4 space-y-2'>
+
+                         <li>Home</li>
+                         <li>About</li>
+                         <li>Blog</li>
+                         <li>Archived</li>
+                         <li>Author</li>
+                         <li>Contact</li>
+
+                       </ul>
+                       </div>
+                                                                                         {/*QUICK LINK KI DIV ABOVE  */}
+                       
+                       <div>
+                         <h1  className='text-white text-[18px] font-semibold'>Category</h1>
+                         <ul className='text-[#BABABF] mt-4 space-y-2'>
+                           <li>Lifestyle</li>
+                           <li>Technology</li>
+                           <li>Travel</li>
+                           <li>Business</li>
+                           <li>Economy</li>
+                           <li>Sports</li>
+                         </ul>
+                       </div>
+                                                                           {/*Category KI DIV ABOVE  */}
+                       <div className='bg-[#242535] rounded-lg p-[32px] w-[390px] h-[254px]'>
+                         <div className='flex flex-col justify-center items-center'>
+                             <h1 className='font-semibold text-white text-[20px]'>Weekly Newsletter</h1>
+                             <p className='font-[#97989F] font-normal text-[#97989F] '>Get blog articles and offers via email</p>
+                         </div>
+                       
+                         <input type="text" placeholder='Your Email' className='rounded-md border-solid border border-[#3B3C4A] text-[#97989F] bg-[#181A2A] block py-[12px] px-[16px] w-[320px] mt-7' />
+                         <button className='rounded-md p-2 text-white font-semibold bg-[#4B6BFB] py-[12px] px-[16px] w-[320px] mt-2'>Subscribe</button>
+
+                       </div>
+                                                                           {/* Weekly Newsletter KI DIV ABOVE  */}
+                       </div>
+                               <hr className="border-t-1 border-[#3B3C4A] w-[1275px] mx-auto mt-10" />
+                       <div className='flex justify-around items-center'>
+                              
+                              
+                               <div className='w-[305px] mt-10 '>
+                                 <h1 className='text-white text-xl '>Meta<span className='text-white font-extrabold'>Blog</span></h1>
+                                 <p className='text-white text-[16px]'>© JS Template <span className='text-[#BABABF]'>2024. All Rights Reserved.</span></p>
+                               </div>
+                                                                           {/* LOGO AND TRADEMARKS */}
+                               <div className='flex items-center text-[#BABABF] mt-[70px]'>
+                                 
+                                 <span>Terms of Use</span>
+                                 <div className='w-[24px] -rotate-90 border-solid border border-[#3B3C4A] mx-2'></div>
+                                 <span  className="">Privacy Policy</span>
+                                 <div className="w-[24px] -rotate-90 border-solid border border-[#3B3C4A] mx-2"></div>
+                                 <span>Cookie Policy</span>
+
+                               </div>
+                                                                           {/* TOS COOKIES ETC */}
+
+                       </div>
+               </div>
+                                                                            {/*  COPYRIGHTS AND ALL RELATED STUFF DIV */}
+
+
+
+
+
+    </div>
+  )
+}
+export default SinglePost
